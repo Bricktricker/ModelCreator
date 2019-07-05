3638b0d6eefac3cf43ac866f8d1dc3299ed08873
Philipp <>
2019-07-05 10:43:15 +0200

    removed old ExporterModel code

--- src/main/java/com/mrcrayfish/modelcreator/ExporterModel.java
+++ src/main/java/com/mrcrayfish/modelcreator/ExporterModel.java
@@ -145,58 +145,6 @@ public class ExporterModel
     	
     	return builder.toJson(root);
     }
-
-    @Deprecated
-    public void write(BufferedWriter writer) throws IOException
-    {
-        writer.write("{");
-        writer.newLine();
-
-        writer.write(space(1) + "\"__comment\": \"Model generated using MrCrayfish's Model Creator (https://mrcrayfish.com/tools?id=mc)\",");
-        writer.newLine();
-        
-        if(displayProps) {
-            writeDisplayProperties(writer);
-            writer.newLine();
-        }else{
-        	writer.write(space(1) + "\"parent\": \"block/block\",");
-        	writer.newLine();
-        }
-
-        if(!((SidebarPanel)manager).getAmbientOcc())
-        {
-            writer.write("\"ambientocclusion\": " + ((SidebarPanel)manager).getAmbientOcc() + ",");
-            writer.newLine();
-        }
-
-        writeTextures(writer);
-        writer.newLine();
-
-        writer.write(space(1) + "\"elements\": [");
-
-        for(int i = 0; i < manager.getElementCount() - 1; i++)
-        {
-            Element element = manager.getElement(i);
-            if(canWriteElement(element))
-            {
-                writeElement(writer, manager.getElement(i));
-                writer.write(",");
-            }
-        }
-        if(manager.getElementCount() > 0)
-        {
-            Element element = manager.getElement(manager.getElementCount() - 1);
-            if(canWriteElement(element))
-            {
-                writeElement(writer, manager.getElement(manager.getElementCount() - 1));
-            }
-        }
-
-        writer.newLine();
-        writer.write(space(1) + "]");
-        writer.newLine();
-        writer.write("}");
-    }
     
     private JsonObject writeTextures() {
     	JsonObject textures = new JsonObject();
@@ -217,47 +165,6 @@ public class ExporterModel
     	});
     	
     	return textures;
-    }
-
-    @Deprecated
-    private void writeTextures(BufferedWriter writer) throws IOException
-    {
-        writer.write(space(1) + "\"textures\": {");
-        writer.newLine();
-        if(((SidebarPanel)manager).getParticle() != null)
-        {
-            TextureEntry entry = ((SidebarPanel)manager).getParticle();
-            writer.write(space(2) + "\"particle\": \"" + this.modid + ":");
-            if(!entry.getDirectory().isEmpty())
-            {
-                writer.write(entry.getDirectory() + "/");
-            }
-            writer.write(entry.getName() + "\"");
-            if(textureMap.size() > 0)
-            {
-                writer.write(",");
-            }
-            writer.newLine();
-        }
-
-        List<String> ids = new ArrayList<>(textureMap.keySet());
-        for(int i = 0; i < ids.size() - 1; i++)
-        {
-            String id = ids.get(i);
-            String texture = textureMap.get(id);
-            writer.write(space(2) + "\"" + id + "\": \"" + texture + "\"");
-            writer.write(",");
-            writer.newLine();
-        }
-        if(ids.size() > 0)
-        {
-            String id = ids.get(ids.size() - 1);
-            String texture = textureMap.get(id);
-            writer.write(space(2) + "\"" + id + "\": \"" + texture + "\"");
-            writer.newLine();
-        }
-
-        writer.write(space(1) + "},");
     }
     
     private JsonObject writeElement(Element cuboid) {
@@ -339,119 +246,6 @@ public class ExporterModel
     	
     	return element;
     }
-
-    @Deprecated
-    private void writeElement(BufferedWriter writer, Element cuboid) throws IOException
-    {
-        writer.newLine();
-        writer.write(space(2) + "{");
-        writer.newLine();
-        if(includeNames)
-        {
-            writer.write(space(3) + "\"name\": \"" + cuboid.getName() + "\",");
-            writer.newLine();
-        }
-        writeBounds(writer, cuboid);
-        writer.newLine();
-        if(!cuboid.isShaded())
-        {
-            writeShade(writer, cuboid);
-            writer.newLine();
-        }
-        if(cuboid.getRotation() != 0)
-        {
-            writeRotation(writer, cuboid);
-            writer.newLine();
-        }
-        writeFaces(writer, cuboid);
-        writer.newLine();
-        writer.write(space(2) + "}");
-    }
-
-    @Deprecated
-    private void writeBounds(BufferedWriter writer, Element cuboid) throws IOException
-    {
-        writer.write(space(3) + "\"from\": [ " + FORMAT.format(cuboid.getStartX()) + ", " + FORMAT.format(cuboid.getStartY()) + ", " + FORMAT.format(cuboid.getStartZ()) + " ], ");
-        writer.newLine();
-        writer.write(space(3) + "\"to\": [ " + FORMAT.format(cuboid.getStartX() + cuboid.getWidth()) + ", " + FORMAT.format(cuboid.getStartY() + cuboid.getHeight()) + ", " + FORMAT.format(cuboid.getStartZ() + cuboid.getDepth()) + " ], ");
-    }
-
-    @Deprecated
-    private void writeShade(BufferedWriter writer, Element cuboid) throws IOException
-    {
-        writer.write(space(3) + "\"shade\": " + cuboid.isShaded() + ",");
-    }
-
-    @Deprecated
-    private void writeRotation(BufferedWriter writer, Element cuboid) throws IOException
-    {
-        writer.write(space(3) + "\"rotation\": { ");
-        writer.write("\"origin\": [ " + FORMAT.format(cuboid.getOriginX()) + ", " + FORMAT.format(cuboid.getOriginY()) + ", " + FORMAT.format(cuboid.getOriginZ()) + " ], ");
-        writer.write("\"axis\": \"" + Element.parseAxis(cuboid.getRotationAxis()) + "\", ");
-        writer.write("\"angle\": " + cuboid.getRotation());
-        if(cuboid.shouldRescale())
-        {
-            writer.write(", \"rescale\": " + cuboid.shouldRescale());
-        }
-        writer.write(" },");
-    }
-
-    @Deprecated
-    private void writeFaces(BufferedWriter writer, Element cuboid) throws IOException
-    {
-        writer.write(space(3) + "\"faces\": {");
-        writer.newLine();
-
-        /* Creates a list of all the valid faces to export */
-        List<Face> validFaces = new ArrayList<>();
-        for(Face face : cuboid.getAllFaces())
-        {
-            if(face.isEnabled() && (includeNonTexturedFaces || face.getTexture() != null) && (!optimize || face.isVisible(manager)))
-            {
-                validFaces.add(face);
-            }
-        }
-
-        /* Writes the valid faces to the writer */
-        for(int i = 0; i < validFaces.size() - 1; i++)
-        {
-            Face face = validFaces.get(i);
-            writeFace(writer, face);
-            writer.write(",");
-            writer.newLine();
-        }
-        if(validFaces.size() > 0)
-        {
-            writeFace(writer, validFaces.get(validFaces.size() - 1));
-        }
-
-        writer.newLine();
-        writer.write(space(3) + "}");
-    }
-
-    @Deprecated
-    private void writeFace(BufferedWriter writer, Face face) throws IOException
-    {
-        writer.write(space(4) + "\"" + Face.getFaceName(face.getSide()) + "\": { ");
-        if(face.getTexture() != null)
-        {
-            writer.write("\"texture\": \"#" + face.getTexture().getKey() + "\"");
-            writer.write(", \"uv\": [ " + FORMAT.format(face.getStartU()) + ", " + FORMAT.format(face.getStartV()) + ", " + FORMAT.format(face.getEndU()) + ", " + FORMAT.format(face.getEndV()) + " ]");
-            if(face.getRotation() > 0)
-            {
-                writer.write(", \"rotation\": " + face.getRotation() * 90);
-            }
-            if(face.isCullfaced())
-            {
-                writer.write(", \"cullface\": \"" + Face.getFaceName(face.getSide()) + "\"");
-            }
-            if(face.isTintIndexEnabled() && face.getTintIndex() >= 0)
-            {
-                writer.write(", \"tintindex\": " + face.getTintIndex());
-            }
-        }
-        writer.write(" }");
-    }
     
     private JsonObject writeDisplayProperties() {
     	JsonObject display = new JsonObject();
@@ -498,54 +292,6 @@ public class ExporterModel
     	return display;
     }
 
-    @Deprecated
-    private void writeDisplayProperties(BufferedWriter writer) throws IOException
-    {
-    	Map<String, DisplayProperties.Entry> entries = BlockManager.displayProperties.getEntries();
-        List<String> ids = new ArrayList<>();
-        for(String id : DISPLAY_PROPERTY_ORDER)
-        {
-            DisplayProperties.Entry entry = entries.get(id);
-            if(entry != null && entry.isEnabled())
-            {
-                ids.add(id);
-            }
-        }
-
-        writer.write(space(1) + "\"display\": {");
-        writer.newLine();
-
-        for(int i = 0; i < ids.size() - 1; i++)
-        {
-            String key = ids.get(i);
-            writeDisplayEntry(writer, key, entries.get(key));
-            writer.write(",");
-            writer.newLine();
-        }
-        if(ids.size() > 0)
-        {
-            String key = ids.get(ids.size() - 1);
-            writeDisplayEntry(writer, key, entries.get(key));
-        }
-
-        writer.newLine();
-        writer.write(space(1) + "},");
-    }
-
-    @Deprecated
-    private void writeDisplayEntry(BufferedWriter writer, String id, DisplayProperties.Entry entry) throws IOException
-    {
-        writer.write(space(2) + "\"" + id + "\": {");
-        writer.newLine();
-        writer.write(space(3) + String.format("\"rotation\": [ %s, %s, %s ],", FORMAT.format(entry.getRotationX()), FORMAT.format(entry.getRotationY()), FORMAT.format(entry.getRotationZ())));
-        writer.newLine();
-        writer.write(space(3) + String.format("\"translation\": [ %s, %s, %s ],", FORMAT.format(entry.getTranslationX()), FORMAT.format(entry.getTranslationY()), FORMAT.format(entry.getTranslationZ())));
-        writer.newLine();
-        writer.write(space(3) + String.format("\"scale\": [ %s, %s, %s ]", FORMAT.format(entry.getScaleX()), FORMAT.format(entry.getScaleY()), FORMAT.format(entry.getScaleZ())));
-        writer.newLine();
-        writer.write(space(2) + "}");
-    }
-
     private boolean canWriteElement(Element element)
     {
         for(Face face : element.getAllFaces())
@@ -556,17 +302,5 @@ public class ExporterModel
             }
         }
         return false;
-    }
-    
-    @Deprecated
-    protected String space(int size)
-    {
-        StringBuilder builder = new StringBuilder();
-        for(int i = 0; i < size; i++)
-        {
-            //TODO add setting to export with tabs instead
-            builder.append("    ");
-        }
-        return builder.toString();
     }
 }
